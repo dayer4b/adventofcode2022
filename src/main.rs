37a1fa860358f52bca8f38e57e50ba26b73fa7e6
Sunil Chopra <sunil@jellyvision.com>
2022-12-02 17:04:16 -0600

    refactored to separate the match logic

--- src/main.rs
+++ src/main.rs
@@ -37,36 +37,40 @@ fn select(args: Args) {
     }
 }
 
-fn day2_part1_values(code: &str) -> i32 {
-    match code {
-        "A X" => 1+3,
-        "B X" => 1+0,
-        "C X" => 1+6,
+fn day2_decode(code: &str, part: &u8) -> i32 {
 
-        "A Y" => 2+6,
-        "B Y" => 2+3,
-        "C Y" => 2+0,
+    match part {
 
-        "A Z" => 3+0,
-        "B Z" => 3+6,
-        "C Z" => 3+3,
-        _ => 0,
-    }
-}
+        1 => match code {
+            "A X" => 1+3,
+            "B X" => 1+0,
+            "C X" => 1+6,
 
-fn day2_part2_values(code: &str) -> i32 {
-    match code {
-        "A X" => 0+3,
-        "B X" => 0+1,
-        "C X" => 0+2,
+            "A Y" => 2+6,
+            "B Y" => 2+3,
+            "C Y" => 2+0,
 
-        "A Y" => 3+1,
-        "B Y" => 3+2,
-        "C Y" => 3+3,
+            "A Z" => 3+0,
+            "B Z" => 3+6,
+            "C Z" => 3+3,
+            _ => 0,
+        },
 
-        "A Z" => 6+2,
-        "B Z" => 6+3,
-        "C Z" => 6+1,
+        2 => match code {
+            "A X" => 0+3,
+            "B X" => 0+1,
+            "C X" => 0+2,
+
+            "A Y" => 3+1,
+            "B Y" => 3+2,
+            "C Y" => 3+3,
+
+            "A Z" => 6+2,
+            "B Z" => 6+3,
+            "C Z" => 6+1,
+            _ => 0,
+        },
+
         _ => 0,
     }
 }
@@ -79,17 +83,9 @@ fn day2(args: Args) {
     let mut total:i32 = 0;
 
     for line in reader.lines() {
-        if args.part == 1 {
-            let points = day2_part1_values(line.as_ref().expect("should be a string").as_ref());
-            println!("{} scores {} points",line.expect("should be a string"), points);
-            total += points;
-        }
-        else if args.part == 2 {
-
-            let points = day2_part2_values(line.as_ref().expect("should be a string").as_ref());
-            println!("{} scores {} points",line.expect("should be a string"), points);
-            total += points;
-        }
+        let points = day2_decode(line.as_ref().expect("should be a string").as_ref(), &args.part);
+        println!("{} scores {} points",line.expect("should be a string"), points);
+        total += points;
     }
 
     println!("The total score is: {}", total);
