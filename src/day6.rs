d957cb09741f1231e4596db489ab92467b4d9a0f
Sunil Chopra <sunil@jellyvision.com>
2022-12-06 08:35:54 -0600

    refactored to reuse code

--- src/day6.rs
+++ src/day6.rs
@@ -3,39 +3,28 @@ use log::info;
 use adventofcode2022::*;
 
 
+fn unique_sequence(size: usize, chars: &Vec<char>) {
+    let slices = chars.windows(size);
+
+    for (iteration, item) in slices.enumerate() {
+        let mut item_vec: Vec<char> = item.to_vec();
+        item_vec.sort();
+        item_vec.dedup();
+        if item_vec.len() == size {
+            info!("{} unique characters found complete after {} characters", item_vec.len(), iteration + size);
+            break;
+        }
+    }
+}
+
 pub fn day6(args: Args) {
 
     let reader = get_input(args.clone());
 
     for line in reader.lines() {
-
         let line = line.unwrap();
-
         let chars: Vec<char> = line.chars().collect();
-        let starting_slices = chars.windows(4);
-        let ending_slices = chars.windows(14);
-
-        for (iteration, item) in starting_slices.enumerate() {
-            let mut item_vec: Vec<char> = item.to_vec();
-            item_vec.sort();
-            item_vec.dedup();
-            if item_vec.len() == 4 {
-                info!("{} unique characters found complete after {} characters", item_vec.len(), iteration + 4);
-                break;
-            }
-        }
-
-        for (iteration, item) in ending_slices.enumerate() {
-            let mut item_vec: Vec<char> = item.to_vec();
-            item_vec.sort();
-            item_vec.dedup();
-            if item_vec.len() == 14 {
-                info!("{} unique characters found complete after {} characters", item_vec.len(), iteration + 14);
-                break;
-            }
-        }
-
-
+        unique_sequence(4, &chars);
+        unique_sequence(14, &chars);
     }
-
 }
