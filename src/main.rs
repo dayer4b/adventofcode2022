9a686b667b84bd49619d0dd9211ede5ddd3bb5a3
Sunil Chopra <sunil@jellyvision.com>
2022-12-06 06:30:41 -0600

    day6 complete!

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,14 @@ use crate::day2::*;
 use crate::day3::*;
 use crate::day4::*;
 use crate::day5::*;
+use crate::day6::*;
 
 mod day1;
 mod day2;
 mod day3;
 mod day4;
 mod day5;
+mod day6;
 
 fn main() {
 
@@ -29,6 +31,7 @@ fn select(args: Args) {
         3 => day3(args.clone()),
         4 => day4(args.clone()),
         5 => day5(args.clone()),
+        6 => day6(args.clone()),
         _ => println!("bad value for day"),
 
     }
